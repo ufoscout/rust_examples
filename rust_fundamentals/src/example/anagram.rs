a9b41e919c5e15d4d5b4381aee4b8a4a295a2e19
ufoscout <ufoscout@gmail.com>
2016-08-21 22:25:47 +0200

    fix return type

--- rust_fundamentals/src/example/anagram.rs
+++ rust_fundamentals/src/example/anagram.rs
@@ -1,5 +1,5 @@
-pub fn anagrams_for(s: &str, inputs: &[&str]) {
-    // Amazing logic TBD
+pub fn anagrams_for<'a>(s: &str, inputs: &[&'a str]) -> Vec<&'a str> {
+    return vec![];
 }
 
 #[test]
@@ -8,4 +8,18 @@ fn should_return_the_anagrams() {
 
     //&inputs is a slice; A slice is a portion of an array whose size is not known at compile time
     anagrams_for("ant", &inputs);
+}
+
+#[test]
+fn test_detect_simple_anagram() {
+  let inputs = ["tan", "stand", "at"];
+  let outputs: Vec<&str> = vec!["tan"];
+  assert_eq!(anagrams_for("ant", &inputs), outputs);
+}
+
+#[test]
+fn test_no_matches() {
+  let inputs = ["hello", "world", "zombies", "pants"];
+  let outputs: Vec<&str> = vec![];
+  assert_eq!(anagrams_for("diaper", &inputs), outputs);
 }
